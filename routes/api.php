4fe6ff128221b4fbd2e501170cd636e53d17b643
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-14 23:12:46 -0300

    espacio entre rutas

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post("/auth",[UserController::class,'Authenticate']);
+
 Route::post("/register",[UserController::class,'Create']);
