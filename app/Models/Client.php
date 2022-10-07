76649087148594f28009cd775e4de375a1141923
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-07 16:22:06 -0300

    soft deletes y cambio de relacion

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,15 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Subscription;
-use App\Models\SubscriptionType;
 use App\Models\User;
 
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'surname',
@@ -24,10 +24,6 @@ class Client extends Model
     }
 
     public function subscription() {
-        return $this->hasOne(Subscription::class);
-    }
-
-    public function subscriptionType() {
-        return $this->hasOneThrough(SubscriptionType::class, Subscription::class);
+        return $this->belongsTo(Subscription::class);
     }
 }
