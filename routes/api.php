09d43ba06ff8897c45d1c114b2a6bb0cee78e216
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-07 16:20:04 -0300

    cambio de comillas y nombres de metodos

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("/auth",[UserController::class,'Authenticate']);
+Route::post('auth',[UserController::class,'authenticate']);
 
-Route::post("/register",[UserController::class,'Create']);
+Route::post('register',[UserController::class,'create']);
